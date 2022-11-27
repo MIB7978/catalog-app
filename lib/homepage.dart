d388199deb8760aa7cc2e1e2c00156efacc0a26b
suraj <sahusuraj7978@gmail.com>
2022-11-27 20:11:42 +0530

    appbar

---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  String name = "Tofan";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Welcome to $days days of flutter by $name"),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
